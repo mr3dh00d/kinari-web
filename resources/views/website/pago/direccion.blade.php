16ff057232a2480db297ac5fb81cc9ebbd393ba2
Dereck Pavez <dereck.pavez@outlook.com>
2022-05-31 16:39:37 -0400

    preparacion vista proceso de pago

---
+++ resources/views/website/pago/direccion.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.base')
+
+@section('title', 'Kinari - Sushi Bar')
+
+@section('idBody', 'select-direccion')
+
+@section('header')
+  <header>
+    @include('website.elements.navbar')
+  </header>
+@endsection
+
+@section('content')
+
+@endsection
+
+@section('footer')
+  @include('website.elements.footer')
+@endsection
